d02956bbe1c4bc448b4999aeb7939068a5da2d63
Christian adon <Adonnda2015@gmail.com>
2023-07-02 18:48:08 +0200

    Sunday react

---
+++ src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export const Header = () => {
+  return (
+    <div>
+        <h1 className= {styles.maintitle}>Welcome to my portfolio</h1>
+    </div>
+  )
+}
