f68908368b6b4ce9e0eab81e152d3704f16601f9
Christian adon <Adonnda2015@gmail.com>
2023-07-05 18:42:26 +0200

    essaie game rock paper scissor

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import { Input, Stack, InputGroup,InputLeftAddon,InputRightAddon } from '@chakra-ui/react'
-
+import { Input, Stack, InputGroup,InputLeftAddon,InputRightAddon,Button, Box} from '@chakra-ui/react'
+import styles from './style.module.css'
 
 export const Content  = () => {
 const [username, setUsername] = useState();
@@ -10,28 +10,40 @@ const HandleChangeUsername = (e) => {
     console.log(e.target.value);
     setUsername(e.target.value);
 }
+const [result, setResult] = useState('');
 
+function getChoices() {
+    const choicesOption = ['pierre', 'feuille', 'ciseaux'];
+    const computerChoice = choicesOption[Math.floor(Math.random() * choicesOption.length)];
+    let playerChoice = prompt("entrer votre choix parmi ces trois pierre, feuille, ciseaux : ");
+
+    while (!choicesOption.includes(playerChoice)) {
+      const playerChoice = prompt("essayer de nouveau, entrer votre choix dans cette liste (pierre, feuille, ciseaux): ");
+    }
+    setResult(checkWin(computerChoice, playerChoice));
+}
+function checkWin(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+      return "Il y a égalité !";
+    } else if (
+      (computerChoice === 'pierre' && playerChoice === 'ciseaux') ||
+      (computerChoice === 'feuille' && playerChoice === 'pierre') ||
+      (computerChoice === 'ciseaux' && playerChoice === 'feuille')
+    ) {
+      return `L'ordinateur gagne ! Il a choisi ${computerChoice}.`;
+    } else {
+      return `Vous gagnez ! Vous avez choisi ${playerChoice}.`;
+    }
+  }
 
   return (
     <div> 
-        <label htmlFor="username">Put your fucking name </label> 
-        <Input type="text" id='username' onChange={HandleChangeUsername}/>
+        <h1 className= {styles.maintitle}>Everybody knows the game Rock, Paper, Scissor, so we don't need to give rules. Use your instinct </h1>
         <br></br>
-
-        <h2>Your fucking name is: <b>{username}</b></h2>
-
-        <Stack spacing={4}>
-            <InputGroup>
-                <InputLeftAddon children='+234' />
-                <Input type='tel' placeholder='phone number' />
-            </InputGroup>
-
-            {/* If you add the size prop to `InputGroup`, it'll pass it to all its children. */}
-            <InputGroup size='sm'>
-                <InputLeftAddon children='https://' />
-                <Input placeholder='mysite' />
-                <InputRightAddon children='.com' />
-            </InputGroup>
+        <Box bg='tomato' w='100%' p={4} color='white' className={styles.maintitle}>Game Pierre-Feuille-Ciseaux </Box>
+        <Stack direction='row' spacing={4} align='center'>
+        <Button colorScheme='teal' onClick={getChoices} size='lg'> Jouer </Button>
+        <p>Résultat: {result}</p>
         </Stack>
     </div>
   )
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,8 @@ import styles from './style.module.css'
 export const Header = () => {
   return (
     <div>
-        <h1 className= {styles.maintitle}>Welcome to my portfolio</h1>
+        <h1 className= {styles.maintitle}>Welcome PFC Game </h1>
+
     </div>
   )
 }
